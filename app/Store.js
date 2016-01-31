aaa98a33c8e221e0ad382af79beefaeac4b64da9
chweeks <chrisharryweeks@gmail.com>
2016-01-31 15:56:30 +0000

    added actions and stores

--- app/Store.js
+++ app/Store.js
@@ -2,11 +2,26 @@ var flux = require('flux-react');
 var actions = require('./actions.js');
 
 module.exports = flux.createStore({
+  users: [],
+  groups: [],
   actions: [
-
+    actions.addUser,
+    actions.addGroup
   ],
-  
+  addUser: function(user) {
+    this.users.push(user);
+    this.emitChange();
+  },
+  addGroup: function(group) {
+    this.groups.push(group);
+    this.emitChange();
+  },
   exports: {
-
+    getUsers: function() {
+      return this.users;
+    },
+    getGroups: function() {
+      return this.groups
+    }
   }
 });
