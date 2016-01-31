61baa3e8aea3ad8fb22d1bda3cc6d17883849079
chweeks <chrisharryweeks@gmail.com>
2016-01-31 16:14:29 +0000

    added render function to app

--- app/App.js
+++ app/App.js
@@ -3,11 +3,15 @@ var Store = require('./Store.js');
 var actions = require('./actions.js');
 
 var App = React.createClass({
-  getInitialState: function () {
-    
-  },
 	render: function() {
-
+    return(
+      <div>
+        <h1>Add New User</h1>
+        <form>
+          <input type="text" placeholder="User Name" />
+        </form>
+      </div>
+    );
 	}
 
 });
